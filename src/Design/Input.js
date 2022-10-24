a295a58a45bd7f4c4e6c321c2bbe02fba83c4fa9
Darshana Perera <darshana.saluka.10@gmail.com>
2022-10-24 17:45:17 +0530

    new item add window created at frontend

---
+++ src/Design/Input.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default function Input() {
+  const [name, setName] = React.useState("");
+  const [des, setDes] = React.useState("");
+
+  return (
+    <div>
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
+      <input
+        value={des}
+        onChange={(e) => {
+          setDes(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          console.log(name + "+" + des);
+          const newItem = {
+            title: name,
+            description: des,
+          };
+        }}
+      >
+        Save
+      </button>
+    </div>
+  );
+}
