d64a56e86597f5650f13d9495c6c258bd7f89c1a
Darshana Perera <91814341+darshana-perera-97@users.noreply.github.com>
2022-10-24 17:14:00 +0530

    backend structure created

--- server.js
+++ server.js
@@ -3,3 +3,18 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const port = 3001;
+
+//connect mongodb
+
+//data schemas
+
+
+//data models
+
+//read route
+
+// create route
+
+//delete route
+
+//update route
